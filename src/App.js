2c892afdbdbd6761e562d5c45f78a4b972e86e70
root <root@LAPTOP-GF8TF1PC>
2023-06-17 03:03:41 +0200

    github actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lab12 <code>src/App.js</code> and save to reload.
+          Łukasz Kałuszyński
         </p>
         <a
           className="App-link"
